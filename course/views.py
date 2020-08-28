f3040ab6ab7013e64aa24299dc87de495ccffa2f
jinho1011 <gino9940@gmail.com>
2020-08-28 21:25:10 +0900

    Add urls

--- course/views.py
+++ course/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class CourseView(View):
+    def get(self, request, *argc, **kwargs):
+        return 'asd'
+        # context = {
+        #     'data': self.data,
+        # }
+        # return render(request, self.template_name, context)
+
+    def post(self, request, *argc, **kwargs):
+        result = request.POST
+        print(result)
+        # context = {
+        #     'data': self.data,
+        #     'result': result,
+        # }
+        # return render(request, self.template_name, context)
